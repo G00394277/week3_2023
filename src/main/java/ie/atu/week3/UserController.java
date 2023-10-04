54032e315a7ab90ceedb83025a622d28198326d3
G00394277 <G00394277@atu.ie>
2023-10-04 15:40:20 +0100

    Initial version

---
+++ src/main/java/ie/atu/week3/UserController.java
@@ -0,0 +1,21 @@
+package ie.atu.week3;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService us) {this.userService = us;}
+
+    @GetMapping("newUser/{name}/{email}")
+    public String getUser(@PathVariable String name, @PathVariable String email)
+    {
+        userService.registerUser(name, email);
+        return "Please wait";
+    }
+}
---
+++ src/main/java/ie/atu/week3/UserService.java
@@ -0,0 +1,19 @@
+package ie.atu.week3;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final EmailService emailService;
+
+    @Autowired
+    public UserService(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    public void registerUser(String username, String email) {
+        emailService.sendEmail(email, "Welcome to our platform, behave or you will be banned");
+    }
+}
